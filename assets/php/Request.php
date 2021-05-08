dba8d15296d14b2132221627d3e50edaff095f06
Carlos Montaño <charlyffs@hotmail.com>
2021-05-08 14:15:17 -0600

    Archivo base de conexión a BD

---
+++ assets/php/Request.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "162.241.62.206";
+$dbUsername = "expoinge_dev";
+$dbPassword = "dev123";
+$dbName = "expoinge_database";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
