d2ed2f617d20d75e9b5674c3054b23281a249610
Carlos Montaño <charlyffs@hotmail.com>
2021-05-11 23:58:45 -0600

    Ahora los filtros se jalan directo de la BD, sin switch.

--- assets/php/RequestCuadricula.php
+++ assets/php/RequestCuadricula.php
@@ -5,29 +5,10 @@ require "conexion.php";
 $result = mysqli_query($con, "SELECT * FROM proyecto");
 
 echo "<div class=\"col-lg-12\">";
-while ($data = mysqli_fetch_assoc($result)) {
-    switch ($data['filtro'][0]) {
-        case 0:
-            $filter = "ACADEMICO";
-            break;
-        case 1:
-            $filter = "SWMC1";
-            break;
-        case 2:
-            $filter = "SWMC2";
-            break;
-        case 3:
-            $filter = "PROCESOC1";
-            break;
-        case 4:
-            $filter = "PROCESOC2";
-            break;
-        case 5:
-            $filter = "PRODUCTOC1";
-            break;
-    }
 
-    echo   "<div class=\"col-lg-3 portfolio-item filter-" . $abbreviation . "\">
+while ($data = mysqli_fetch_assoc($result)) {
+
+    echo   "<div class=\"col-lg-3 portfolio-item filter-" . $data['filtro'] . "\">
                 <div class=\"portfolio-wrap\">
                     <div class=\"image-container\">
                         <img src=\"assets/img/map/1.png\" class=\"img-fluid\" alt=\"\">
@@ -35,7 +16,6 @@ while ($data = mysqli_fetch_assoc($result)) {
                     </div>
                     <div class=\"portfolio-info\">
                         <h4>" . $data['nombre'] . "</h4>
-                        <p>" . $data['nombre'] . "</p>
                         <div class=\"portfolio-links\">
                             <a href=\"proyecto.html\" title=\"Conocer más\"><i class=\"icon-arrow-right-circle\"></i></a>
                         </div>
@@ -43,4 +23,5 @@ while ($data = mysqli_fetch_assoc($result)) {
                 </div>
             </div>\n";
 }
+
 echo "</div>";
